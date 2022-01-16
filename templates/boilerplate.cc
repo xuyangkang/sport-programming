e854ba9f023cdbc3a28aa131ffc533579f2c6142
Xuyang Kang <xuyangkang@gmail.com>
2022-01-17 02:17:13 +0900

    Add a boilerplate cc template

---
+++ templates/boilerplate.cc
@@ -0,0 +1,21 @@
+#include <cstdio>
+#define CF_MULTI_CASES
+
+void solve();
+
+int main() {
+#ifdef CF_MULTI_CASES
+    int cases;
+    scanf("%d", &cases);
+    while (cases--) {
+        solve();
+    }
+#else
+    solve();
+#endif
+    return 0;
+}
+
+
+void solve() {
+}
