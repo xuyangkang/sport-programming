84f895ea057fe0f80e7608cdfc90eb14ddc4e735
Xuyang Kang <xuyangkang@gmail.com>
2024-01-28 15:50:10 +0000

    use bazel and gtest

--- cpp/test.cc
+++ cpp/test.cc
@@ -1,7 +1,18 @@
+#include "./boilerplate.cc" // NOLINT
+
 #define USE_AS_HEADER
 
-#include "./boilerplate.cc" // NOLINT
 #include <cassert>
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
 
 // TODO(xuyang): a proper C++ test framework
 
